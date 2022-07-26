20b778471dd76d882d9e871617515d7bb7aaab9d
Heber Marques <heber.marques@umusic.com>
2022-07-26 17:40:56 -0300

    Added lambda function

---
+++ SearchWorker/SearchWorker.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.Json;
+using Amazon.Lambda.SNSEvents;
+
+namespace SearchWorker;
+
+[assembly: LambdaSerializer(typeof(JsonSerializer))]
+public class SearchWorker
+{
+    public void Function(SNSEvent snsEvent, ILambdaContext context)
+    {
+        foreach (var record in snsEvent.Records)
+        {
+            context.Logger.LogLine(record.Sns.Message);
+        }
+    }
+}
